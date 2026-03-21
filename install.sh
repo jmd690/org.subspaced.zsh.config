2ac3b928afb14d3868235b3418f1905093600292
jmd690 <jmd012616@gmail.com>
2026-03-21 13:20:01 -0500

    Add installation script for Oh My Posh and Zsh plugins

---
+++ install.sh
@@ -0,0 +1,32 @@
+
+
+```bash
+# Install Oh My Posh (Ubuntu/Debian example)
+sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+sudo chmod +x /usr/local/bin/oh-my-posh
+
+# Download Night Owl theme to your home folder
+curl -o ~/night-owl.omp.json https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/night-owl.omp.json
+
+# Install plugins (create dir first if needed)
+mkdir -p ~/.zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.zsh/zsh-syntax-highlighting
+
+# Backup your current config first!
+[ -f ~/.zshrc ] && cp ~/.zshrc ~/.zshrc.backup-$(date +%F)
+
+# Install and apply
+git clone https://github.com/jmd690/org.subspaced.zsh.config.git
+cd org.subspaced.zsh.config
+# Do a quick ls to make sure zshrc is there
+ls
+# If it is, then :
+cp zshrc ~/.zshrc
+# Then apply
+exec zsh
+#(or source ~/.zshrc or just zsh if u prefer)
+#!Then u should be done!
+echo "done (bye, bye and potato)
+# Want to update later?
+To update later: cd into the folder → git pull → source ~/.zshrc
